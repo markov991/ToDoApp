84c4920d6e856d063f8dcf21e7e0411f860a7084
markov991 <markov991@gmail.com>
2022-12-12 14:28:55 +0100

    added funcionality to events(rendering tasks by event)

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const addbutton = document.querySelector(".add-new-task");
 const modal = document.querySelector(".modal");
 const defaultevents = document.querySelector(".default");
-const events = [];
+const eventsContainer = document.querySelector(".events");
+const events = ["Frontend"];
 const tasks = [
   {
     taskName: "Learn React",
@@ -36,7 +37,7 @@ const tasks = [
     status: "finished",
   },
   {
-    taskName: "Learn React",
+    taskName: "Learn Python",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus odit quos in reprehenderit ipsum maiores",
     date: "2020-06-08",
@@ -67,12 +68,92 @@ const collectingDataForTasks = function () {
         description: taskDescription.value,
         date: dateInput.value,
         eventName: eventInput.value,
+        status: "ongoing",
       });
-      events.push(eventInput.value);
+      if (!events.includes(eventInput.value)) {
+        events.push(eventInput.value);
+      }
       modal.classList.toggle("hidden");
+      eventsContainer.innerHTML = "";
+      renderingEvents();
     });
   }
 };
+const renderingEvents = function () {
+  events.forEach((event) => {
+    eventsContainer.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="event">${event}</div>`
+    );
+  });
+  const event = eventsContainer.getElementsByTagName("div");
+
+  for (let i = 0; i < event.length; i++) {
+    event[i].addEventListener("click", () => {
+      document.querySelector(".main-bar").innerHTML = "";
+      renderingTaskContainers();
+
+      tasks.forEach((task) => {
+        if (
+          event[i].innerText === task.eventName
+          // &&
+          // task.status === "ongoing"
+        ) {
+          console.log(task.taskName);
+          renderingTasks(
+            document.querySelector(`.${task.status}-tasks-containter`),
+            task.status,
+            task.taskName,
+            task.description
+          );
+        }
+      });
+    });
+  }
+  // console.log(x);
+};
+const renderingTaskContainers = function () {
+  document.querySelector(".main-bar").insertAdjacentHTML(
+    "afterbegin",
+    `
+  <div class="ongoing-list">
+    <h3 class="task-header">ONGOING TASKS</h3>
+    <div class="ongoing-tasks-containter"></div>
+    
+</div>
+<div class="finished-list">
+    <h3 class="task-header">FINISHED TASKS</h3>
+    <div class="finished-tasks-containter"></div>
+    
+</div>
+
+<div class="unfinished-list">
+<h3 class="task-header">UNFINISHED TASKS</h3>
+<div class="unfinished-tasks-containter"></div>
+
+</div>
+`
+  );
+};
+
+const renderingTasks = function (location, status, taskName, taskDescription) {
+  location.insertAdjacentHTML(
+    "beforeend",
+    `
+  
+    
+      <div class="${status}-task">
+        <h4 class="task-name">${taskName}</h4>
+        <div class="task-description">
+          <p>${taskDescription}
+            
+          </p>
+        </div>
+      </div>
+    </div>`
+  );
+};
+addEventListener("load", () => renderingEvents());
 const renderingTaskForm = function () {
   modal.insertAdjacentHTML(
     "afterbegin",
